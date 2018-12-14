aca56b739d32bf23b88e3d4c865b3a81aa4090f4
simpson <bok@dreamwiz.com>
2018-12-15 07:27:58 +0900

    mydev edit

--- main.c
+++ main.c
@@ -2,32 +2,5 @@
 
 void Show(float v)
 {
-	float n = v;
-	printf("main. 1");
-	printf("main. 2");	
-	printf("main. 3");
-	printf("main. 4");
-	printf("main. 5");
+    mydev();
 }
-
-void main()
-{
-	Show(0);
-} 
-
-void showChild()
-{
-	//123
-	//456
-	//789
-}
-
-void showChild2()
-{
-	printf("aaaa");
-	printf("bbb");
-	printf("ccc");
-}
-//Kraken Test
-//Kraken modify
-//This is main
